718164cf3f61990a08ac15822bb81a98ddcd9c58
vzt7 <vzt7.zed@gmail.com>
2023-06-29 23:37:02 +0800

    feat: 🎸 Accept to customize separator
    BREAKING CHANGE: 🧨 Changed fragments.fragments() behavior which return the code fragments
    and not be push directly.

--- src/CodeFragments.spec.ts
+++ src/CodeFragments.spec.ts
@@ -23,7 +23,7 @@ describe('Testing code fragments.', () => {
 
     fragments.push(items.str1);
     fragments.newline();
-    fragments.fragments(items.str2, items.str3);
+    fragments.push(fragments.fragments(items.str2, items.str3));
 
     const code = fragments.complete();
     expect(code).eq(`${items.str1}\n\n\n  ${items.str2}\n  ${items.str3}`);
@@ -34,7 +34,7 @@ describe('Testing code fragments.', () => {
 
     fragments.push(items.str1);
     fragments.newline();
-    fragments.fragments(items.str2, items.str3);
+    fragments.push(fragments.fragments(items.str2, items.str3));
 
     const code = fragments.complete({ indent: false });
     expect(code).eq(`${items.str1}\n\n\n${items.str2}\n${items.str3}`);
@@ -45,12 +45,48 @@ describe('Testing code fragments.', () => {
 
     fragments.push((ctx) => `${ctx.prefix}${items.str1}`);
     fragments.newline();
-    fragments.fragments(items.str2, (ctx) => `${ctx.prefix}${items.str3}$$$`);
+    fragments.push(
+      fragments.fragments(items.str2, (ctx) => `${ctx.prefix}${items.str3}$$$`)
+    );
 
     const code = fragments.complete({
       context: { prefix: '@@@' },
       indent: false,
     });
     expect(code).eq(`@@@${items.str1}\n\n\n${items.str2}\n@@@${items.str3}$$$`);
+  });
+
+  it('Custom separator (string)', () => {
+    const fragments = new CodeFragments();
+
+    fragments.push(items.str1);
+    fragments.push(items.str2);
+    fragments.push(items.str3);
+
+    const separator = '#####';
+    const code = fragments.complete({
+      separator,
+    });
+    expect(code).eq(
+      `${items.str1}${separator}${items.str2}${separator}${items.str3}`
+    );
+  });
+
+  it('Custom separator (function)', () => {
+    const fragments = new CodeFragments();
+
+    fragments.push(items.str1);
+    fragments.push(
+      fragments.fragments(items.str2, new CodeFragments(items.str2))
+    );
+    fragments.push(items.str3);
+
+    const code = fragments.complete({
+      indent: false,
+      separator: ({ depth }) => `[${depth}]`,
+    });
+    expect(code).eq(
+      `${items.str1}[0]${items.str2}[1]${items.str2}[0]${items.str3}`
+    );
   });
 });
--- src/CodeFragments.ts
+++ src/CodeFragments.ts
@@ -14,7 +14,7 @@ export class CodeFragments<Context = any> extends Array<Fragments<Context>> {
   }
 
   fragments(...args: Fragments<Context>[]) {
-    this.push(new CodeFragments(...args));
+    return new CodeFragments(...args);
   }
 
   private _complete(
@@ -26,6 +26,7 @@ export class CodeFragments<Context = any> extends Array<Fragments<Context>> {
       endline?: boolean;
       depth?: number;
       spaces?: number;
+      separator?: string | ((options: { depth: number }) => string);
     }
   ): string {
     const {
@@ -35,6 +36,7 @@ export class CodeFragments<Context = any> extends Array<Fragments<Context>> {
       endline = false,
       depth = 0,
       spaces = 2,
+      separator,
     } = options || {};
     const _fragments = [...fragments];
     if (startline && _fragments[0] !== '\n') {
@@ -44,33 +46,32 @@ export class CodeFragments<Context = any> extends Array<Fragments<Context>> {
       _fragments.push('\n');
     }
 
-    const separator = '\n' + (indent ? ' '.repeat(spaces * depth) : '');
+    const getCode = (item: (typeof _fragments)[number]) => {
+      if (typeof item === 'string') {
+        return item;
+      }
+      if (item instanceof CodeFragments) {
+        const padSpaces = indent ? ' '.repeat(spaces * (depth + 1)) : '';
+        return padSpaces + item.complete({ ...options, depth: depth + 1 });
+      }
+      if (typeof item === 'function') {
+        return item(context);
+      }
+      return this._complete(item, { ...options, depth: depth + 1 });
+    };
+    const getSeparator = () => {
+      const _separator =
+        separator ?? '\n' + (indent ? ' '.repeat(spaces * depth) : '');
+      if (typeof _separator === 'function') {
+        return _separator({ depth });
+      }
+      return _separator;
+    };
 
-    return _fragments
-      .map((item) => {
-        if (typeof item === 'string') {
-          return item;
-        }
-        if (item instanceof CodeFragments) {
-          const padSpaces = indent ? ' '.repeat(spaces * (depth + 1)) : '';
-          return padSpaces + item.complete({ ...options, depth: depth + 1 });
-        }
-        if (typeof item === 'function') {
-          return item(context);
-        }
-        return this._complete(item, { ...options, depth: depth + 1 });
-      })
-      .join(separator);
+    return _fragments.map((item) => getCode(item)).join(getSeparator());
   }
 
-  complete(options?: {
-    context?: Context;
-    indent?: boolean;
-    startline?: boolean;
-    endline?: boolean;
-    depth?: number;
-    spaces?: number;
-  }) {
+  complete(options?: Partial<Parameters<typeof this._complete>['1']>) {
     return this._complete(this, { context: {} as Context, ...options });
   }
 }
